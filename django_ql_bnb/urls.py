21aef017eb2eac03ecdea8229eb6cc7a184e229d
QianjunL <qianjunliang.ql@gmail.com>
2024-09-12 23:19:32 -0600

    finish registration

--- django_ql_bnb/urls.py
+++ django_ql_bnb/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/', include('properties.urls')),
+    path('api/auth/', include('user_accounts.urls') ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
